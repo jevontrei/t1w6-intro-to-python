29992a8a5d3b42fcb747a175a43ea0163d7a3a5f
jevontrei <160815182+jevontrei@users.noreply.github.com>
2024-06-04 21:02:00 +1000

    programs with inputs

---
+++ area_of_square.py
@@ -0,0 +1,9 @@
+# get the length from user
+length = float(input("Enter the side length of the square: "))
+print(length)
+
+# calculate and store area in a variable3
+area = length ** 2
+
+# print the calculated area to the terminal
+print(f"The area is {area})
---
+++ create_email_form.py
@@ -0,0 +1,9 @@
+# Joel von Treifeldt -> joel.von treifeldt@coderacademy.com
+
+first_name = input("Enter your first name (no blank spaces): ")
+last_name = input("Enter your last name (no blank spaces): ")
+company_name = input("Enter your company name (no blank spaces): ")
+print(f"{first_name}.{last_name}@{company_name}.com".lower())
+
+email = f"{first_name}.{last_name}@{company_name}.com".lower()
+print(email)
