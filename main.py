152a86fcb50e879e8ddb72716a96437d03aa04c6
jevontrei <160815182+jevontrei@users.noreply.github.com>
2024-06-04 20:29:44 +1000

    data types

--- main.py
+++ main.py
@@ -1,3 +1,69 @@
-print("Hello world!")
-print(5-3)
-print("Don't forget to commit!")
+# print("Hello world!")
+# print(5-3)
+# print("Don't forget to commit!")
+
+# print("It's a comment, it won't be printed")
+
+# Data types
+# Integers
+# 4
+# 7
+# print(9)
+# print("9")
+# print(type(9))
+# print(type("9"))
+
+# Floats
+# 2.5
+# 5.1245
+# print(3.1415)
+# print(type(-5.76))
+# print(type(-5))
+
+# Complex / imaginary numbers
+# 2 + 3j
+# print(type(2+3j))
+
+# Operators: + - * / %
+# print (4 * 5)
+# print(15 / 3)
+# print(15 / 2)
+# print(7 % 2)
+
+# Strings
+# "Hello world"
+# 'Hello'
+# Better to use double quotes
+# print("This is a string")
+
+# String concatenation, addition, join
+# print("Hello " + "world")
+# print(3 + 5)
+# print("3" + "5")
+
+# Booleans
+# True
+# False
+# Must be capitalised
+# print(type(False))
+0 # falsy (AKA falsey) value
+"" # falsy value
+# truthy haha
+# print(bool(0))
+# print(bool(1))
+# print(bool(""))
+# print(bool(-123))
+# print(bool("False"))
+
+# Comparison operators: < > =< => == !=
+# print (2 < 3)
+# print(5 >= 5)
+# print("Hello".lower() == "hello")
+
+
+# Logical operators: and, or, not
+print(True and False)  # AND returns True when both operators(?) are True
+print(False or True)  # OR returns True when one or both are true
+print(not False)  # not returns the opposite value
+
+
